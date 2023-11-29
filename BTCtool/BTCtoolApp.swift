b8d2b2462aa0431ae4d8e832ed81ff6461e36e6f
ajv <voisine@gmail.com>
2023-11-29 10:52:59 -0800

    XCode project

---
+++ BTCtool/BTCtoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  BTCtoolApp.swift
+//  BTCtool
+//
+//  Created by ajv on 11/29/23.
+//
+
+import SwiftUI
+
+@main
+struct BTCtoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
