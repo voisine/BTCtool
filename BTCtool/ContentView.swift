addf14a25c5b5aa627abddb2fcd8cf9cb949296c
ajv <voisine@gmail.com>
2023-11-29 18:00:34 -0800

    swift wrapper experiment

--- BTCtool/ContentView.swift
+++ BTCtool/ContentView.swift
@@ -7,13 +7,22 @@
 
 import SwiftUI
 
+func base58Encode(str: String) -> String? {
+    var buf = Array<Int8>(repeating: 0, count: 2048)
+    return str.withCString { cString in
+        ZNBase58Encode(&buf, buf.count, cString, str.count)
+        return String(validatingUTF8:buf)
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            //Text("Hello, world!")
+            Text(base58Encode(str:"Hello, world!")!)
         }
         .padding()
     }
