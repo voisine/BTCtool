06fc837aae20dccba86aeb66c5fb5fe08b0c5bd4
ajv <voisine@gmail.com>
2023-11-30 21:31:52 -0800

    qr scanning

--- BTCtool/BTCtoolApp.swift
+++ BTCtool/BTCtoolApp.swift
@@ -7,6 +7,14 @@
 
 import SwiftUI
 
+func base58Encode(str: String) -> String? {
+    var buf = Array<Int8>(repeating: 0, count: 2048)
+    return str.withCString { cString in
+        ZNBase58Encode(&buf, buf.count, cString, str.count)
+        return String(validatingUTF8:buf)
+    }
+}
+
 @main
 struct BTCtoolApp: App {
     var body: some Scene {
--- BTCtool/ContentView.swift
+++ BTCtool/ContentView.swift
@@ -7,26 +7,31 @@
 
 import SwiftUI
 
-func base58Encode(str: String) -> String? {
-    var buf = Array<Int8>(repeating: 0, count: 2048)
-    return str.withCString { cString in
-        ZNBase58Encode(&buf, buf.count, cString, str.count)
-        return String(validatingUTF8:buf)
-    }
-}
-
 struct ContentView: View {
-    var str = base58Encode(str:"Hello, world!")!
+    @State private var isPresentingScanner = false
+    @State private var scannedCode: String? = base58Encode(str:"Hello, world!")!
     var body: some View {
         VStack {
             //Image(systemName: "globe")
             //    .imageScale(.large)
             //    .foregroundStyle(.tint)
-            Image(uiImage: UIImage.qrCode(data: str.data(using: .utf8)!)!.resize(CGSize(width: 300, height: 300))!)
+            Image(uiImage: UIImage.qrCode(data: (scannedCode ?? "").data(using: .utf8)!)!
+                .resize(CGSize(width: 300, height: 300))!)
             //Text("Hello, world!")
-            Text(str)
+            Text(scannedCode ?? "[]")
+            Button("Scan QR") { isPresentingScanner = true }
+            .padding()
         }
+        .buttonStyle(.bordered)
         .padding()
+        .sheet(isPresented: $isPresentingScanner) {
+            CodeScannerView(codeTypes: [.qr], showViewfinder: true, simulatedData: "Hello, world!") { response in
+                if case let .success(result) = response {
+                    scannedCode = result.string
+                    isPresentingScanner = false
+                }
+            }
+        }
     }
 }
 
